f2e05adb1a2d762d45927bbb48b3e4e05b93e321
Syifa Andini <syifandin@gmail.com>
2020-06-11 21:39:19 +0700

    submit form

--- index.js
+++ index.js
@@ -31,11 +31,31 @@ $("#kalimat").on("keydown", function(e){
     $("#kirimKalimat").click();
     return false;
   }
+
 });
 
 //Get Name
 $("form").submit(function(event){
   var namaOrang = $("input").val();
   var ucapan = $("textarea").val();
-  alert(namaOrang + "; " + ucapan);
+  // alert(namaOrang + "; " + ucapan);
 });
+
+// Submit ke Gsheet
+
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbzmujn9ot-JjJ_O_LjkfmMvsDpLyxEYwzgXXmN2blDTl1e0Nkg/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    alert("yey");
+  );
+})
+
+// https://script.google.com/macros/s/AKfycbzmujn9ot-JjJ_O_LjkfmMvsDpLyxEYwzgXXmN2blDTl1e0Nkg/exec?nama=rama&pesan=woi
